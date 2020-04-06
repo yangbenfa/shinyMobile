69bb41527a9543c69f265455a2e1532123da3cba
DivadNojnarg <dgranjon@ymail.com>
2020-04-07 01:38:56 +0200

    new strategy

--- inst/framework7-5.5.0/my-app.js
+++ inst/framework7-5.5.0/my-app.js
@@ -2,7 +2,11 @@ $(function () {
   // handles shinyapps.io
   var workerId = $('base').attr('href');
   if (workerId) {
-    $('a[href^="#"].tab-link').attr('href', workerId + $('a[href^="#"].tab-link').attr('href'));
+    var tabLinks = $('a[href^="#"].tab-link');
+    for (var i = 0; i < tabLinks.length; i++) {
+      var temp = tabLinks[i].attr('href');
+      tabLinks[i].attr('href', temp.split('/')[1]);
+    }
   }
 
   // handle toolbar padding for mobiles in standalone mode
